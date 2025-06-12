7597284b170d326972c08dff433373ce88f738f4
jesulas <77580179+jesulas@users.noreply.github.com>
2025-06-12 13:53:16 +0100

    final update

--- src/pages/nueva-cuenta/creacion-cuenta.page.tsx
+++ src/pages/nueva-cuenta/creacion-cuenta.page.tsx
@@ -1,41 +1,41 @@
 import { AppLayout } from "@/layouts/app/app-layout";
 import React from "react";
 import { Account } from "./creacion-cuenta.vm";
-import { SubidaCuenta } from "./creacion-subida";
+import { SubidaCuenta } from "./api/creacion-subida";
+import classes from "../nueva-cuenta/creacion-cuenta.page.module.css"
+
 
 export const CreacionCuentaPage: React.FC = () => {
 
 const [account, setAccount] = React.useState<Account>({ type: "", name: ""});
-const [nombre, setNombre] = React.useState<string>("");
-const [tipe, setType] = React.useState<string>("");
+
 
     const convertirCuenta = (event: { preventDefault: () => void; }) => {
         event.preventDefault()
-        setAccount(
-            {
-            name : nombre,
-            type : tipe
-           }
-        )
-
         SubidaCuenta(account)
     }
 
     return (
         <AppLayout> 
-            <div>
-               <h1>Movement List</h1> 
+            <div className={classes.headerContainer}>
+               <h1 className={classes.headerText}>Movement List</h1> 
             </div>
-            <form onSubmit={convertirCuenta}>
-            <p>Tipo de cuenta:</p>
-            <select name="tQuery" required onChange ={(f) => setType(f.target.value)} value={tipe}>
-                <option value={"0"}>Escoge un tipo:</option>
-                <option value={"1"}>Tipo 1</option>
-                <option value={"2"}>Tipo 2</option>
-                <option value={"3"}>Tipo 3</option>
-            </select>
-            <p>Alias:</p>
-            <input name="nQuery" type="text" onChange={(e) => setNombre(e.target.value)} value={nombre}/>
+            <form onSubmit={convertirCuenta} className={classes.formContainer}>
+               <div>
+                 <div className={classes.formElement}>
+                    <p>Tipo de cuenta:</p>
+                    <select name="tQuery" className={classes.formSelect} required onChange ={(f) => setAccount({...account, type: f.target.value})} value={account.type}>
+                        <option value={"0"}>Escoge un tipo:</option>
+                        <option value={"1"}>Tipo 1</option>
+                        <option value={"2"}>Tipo 2</option>
+                        <option value={"3"}>Tipo 3</option>
+                    </select>
+                </div>
+                <div className={classes.formElement}>
+                    <p>Alias:</p>
+                    <input name="nQuery" type="text" onChange={(e) => setAccount({...account, name: e.target.value})} value={account.name}/>
+                </div>
+            </div>
             <button type="submit" value="">Guardar</button>
             </form>
         </AppLayout>
